8c197963d13ca18fa8884a0076aeceb2623d2f47
Sinan AlSheikh <la7oon@gmail.com>
2013-03-10 22:30:23 +0400

    updated AMO_chmod_cakephp_tmp_folder and added AMO_create_htaccess recipe

---
+++ AMO/recipes/AMO_create_htaccess.rb
@@ -0,0 +1,9 @@
+file "/srv/www/amo/current/.htaccess" do
+  content "<IfModule mod_rewrite.c>
+   RewriteEngine on
+   RewriteRule    ^$ app/webroot/    [L]
+   RewriteRule    (.*) app/webroot/$1 [L]
+</IfModule>"
+  action :create
+  Chef::Log.info("Sinan Just created .htaccess file for AMO application")
+end
--- AMO/recipes/AMO_restart.rb
+++
@@ -1,6 +0,0 @@
-include_recipe "apache2::service"
-
-service "apache2" do
-  Chef::Log.info("Sinan Just Restarted Apache2")
-  action :restart
-end
